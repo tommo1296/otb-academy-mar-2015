0f9ac4d3447038c35e501930ca66daba540e04ef
Tom <thomas.altree@onthebeach.co.uk>
2015-03-11 17:17:32 +0000

    Add basket class

--- harry_potter/lib/book_shop.rb
+++ harry_potter/lib/book_shop.rb
@@ -1,18 +1,29 @@
-class BookShop
+class Basket
+  attr_reader :price
 
   def initialize
-    @basket = 0
+    @price = 0.00
+    @books = []
+  end
+
+  def add(book_id)
+    @books << book_id
+    @price += 8.00
+  end
+
+end
+
+class BookShop
+  attr_reader :basket
+
+  def initialize
+    @basket = Basket.new
   end
 
   def create_basket(items = [])
-    @basket_items = items
-  end
-
-  def basket
-    @basket_items.each do |item|
-      @basket += 8.00
+    items.each do |item|
+      @basket.add(item)
     end
-    return @basket
   end
 
 end
--- harry_potter/spec/harry_potter_spec.rb
+++ harry_potter/spec/harry_potter_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe "Harry potter shop" do
   
     it "should give price for any 1 book" do
       book_shop.create_basket([1])
-      expect(book_shop.basket).to eq(8.00)
+      expect(book_shop.basket.price).to eq(8.00)
     end
 
     it "should give price for multiple copies" do
       book_shop.create_basket((1..5).map { 1 }) #create 5 books of book 1
-      expect(book_shop.basket).to eq(40.00)
+      expect(book_shop.basket.price).to eq(40.00)
     end
 
   end
