e82e93fd8837ec33cefb2ddbbb16e0ad8c648dfd
Tom <thomas.altree@onthebeach.co.uk>
2015-03-11 16:37:21 +0000

    Add costing of multiple books of the same type ONLY

--- harry_potter/spec/harry_potter_spec.rb
+++ harry_potter/spec/harry_potter_spec.rb
@@ -4,11 +4,16 @@ RSpec.describe "Harry potter shop" do
 
   let(:book_shop) { BookShop.new }
 
-  context "when buying multple copies of the same book"
+  context "when buying multple copies of the same book" do
   
     it "should give price for any 1 book" do
       book_shop.create_basket([1])
       expect(book_shop.basket).to eq(8.00)
+    end
+
+    it "should give price for multiple copies" do
+      book_shop.create_basket((1..5).map { 1 }) #create 5 books of book 1
+      expect(book_shop.basket).to eq(40.00)
     end
 
   end
