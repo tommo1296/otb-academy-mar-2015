b1b42a4383f1c84ccfd34c0bbd25c6ca28e2f53a
Tom <altree.thomas@googlemail.com>
2015-03-12 00:25:46 +0000

    Make price a calculated value off book quantity

--- harry_potter/lib/book_shop.rb
+++ harry_potter/lib/book_shop.rb
@@ -1,16 +1,29 @@
 class Basket
-  attr_reader :price
 
   def initialize
     @price = 0.00
     @books = []
-    @qtys = (0..4).map { 0 }
+    @qtys = [0,0,0,0,0]
   end
 
   def add(book_id)
     @books << book_id
-    @price += 8.00
+    update_quantity(book_id)
   end
+
+  def price
+    @price = @qtys.reduce(:+) * 8.00
+
+    
+
+    return @price
+  end
+
+  private
+
+    def update_quantity(book_id)
+      @qtys[book_id - 1] += 1
+    end
 
 end
 
